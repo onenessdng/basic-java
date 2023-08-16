00eaec25bb9d7eb4d1986752a299f3a1a3136444
onenessdng <100875940+onenessdng@users.noreply.github.com>
2023-08-17 03:51:43 +0530

    Add files via upload

---
+++ Kush.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Kush {
+    public static void main(String[] args) {
+        System.out.println("hello piyush");
+        // Scanner input=new Scanner(System.in);
+        // int p=input.nextInt();
+        // int c=input.nextInt();
+        // // System.out.println(input.next());
+//         int sum=Sum();
+//         System.out.println(sum);
+
+//     }
+//   static int Sum() {
+
+// Scanner sc =new Scanner(System.in);
+// int a=sc.nextInt();
+// int b=sc.nextInt();
+// int sum4=a+b;
+// // System.out.println("The sum is\n" + sum4);
+// return sum4;
+  Scanner in=new Scanner(System.in);
+  System.out.println("Enter the two numbers:");
+  int num1=in.nextInt();
+  int num2=in.nextInt();
+  // int sum=in.nextInt();
+   int sum,mul;
+   sum=num1+num2;
+   mul=num1*num2;
+  System.out.println("The sum is :"); 
+  System.out.println(sum);
+  System.out.println("The product is:");
+  System.out.println(mul);
+
+  
+
+
+  } 
+}
